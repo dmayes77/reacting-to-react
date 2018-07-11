181d77cc5b78cd1614a93ff5e877cf3f6d58374a
David Mayes <dmayes77@gmail.com>
2018-07-11 14:23:22 -0400

    Completed Lab: Reacting to React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,15 +9,46 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			text: 'Hello World'
+			text: 'Hello World',
+			hasLoaded: false,
+			name: ''
 		};
 	}
+	handleInput = value => {
+		this.setState({ name: value });
+	};
+
+	handleClick = () => {
+		this.setState({ hasLoaded: !this.state.hasLoaded });
+	};
+
+	componentDidMount = () => {
+		this.setState({ hasLoaded: true });
+	};
+
 	render() {
-		return (
-			<h1>
-				My name is {this.props.name}, {this.state.text}.
-			</h1>
-		);
+		if (this.state.hasLoaded) {
+			return (
+				<React.Fragment>
+					<h1>
+						My name is {this.props.name} {this.state.name}, {this.state.text}.
+					</h1>
+					<button onClick={this.handleClick}> Submit </button>
+					<input
+						placeholder="What's your name?"
+						value={this.state.name}
+						onChange={event => this.handleInput(event.target.value)}
+					/>
+				</React.Fragment>
+			);
+		} else {
+			return (
+				<React.Fragment>
+					<h1>Loading...</h1>
+					<button onClick={this.handleClick}> Submit </button>
+				</React.Fragment>
+			);
+		}
 	}
 }
 
